bcf788687b443aa6aef58dcd7813a080e380bba4
Divakar Verma <divakarv96@gmail.com>
2021-12-27 09:26:46 -0500

    First commit. 2 big-sized graphs - Curves, Intersection

---
+++ app.py
@@ -0,0 +1,54 @@
+from matplotlib import widgets
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+import plotly.express as px
+
+from utils import *
+
+col1,col2 = st.columns(2)
+
+N = 500
+data1 = np.array([list(generate_F1_sample()) for i in range(N)])
+data2 = np.array([list(generate_F2_sample()) for i in range(N)])
+
+x, y, z = data1[:, 0], data1[:, 1], data1[:, 2]
+trace1 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F1", marker=dict(
+            # color=px.colors.qualitative.D3,
+            size=5,sizemode='diameter'))
+x, y, z = data2[:, 0], data2[:, 1], data2[:, 2]
+trace2 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F2", marker=dict(
+            # color=px.colors.qualitative.D3,
+            size=5,sizemode='diameter'))
+
+fig = make_subplots()
+fig.add_trace(trace1)
+fig.add_trace(trace2)
+fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+
+fig.update_layout(height=800, width=1000)
+with col1:
+    st.plotly_chart(fig, use_container_width=True, height = 800, width=1000)
+
+## Plot for Intersection of Curves
+data = [list(generate_sample()) for i in range(N)]
+data = np.array(data)
+x, y, z = data[:, 0], data[:, 1], data[:, 2]
+fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z,mode='markers',marker=dict(
+            size=5,sizemode='diameter'))])
+fig.update_layout(height=800, width=800)
+fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+
+with col2:
+    st.plotly_chart(fig, use_container_width=True, height = 800, width=800)
+
+
+st.write(F1_curve(X))
+st.write(F2_curve(X))
+st.write(G(X))
+
+st.write(y_F1_prime_mat)
---
+++ utils.py
@@ -0,0 +1,105 @@
+import numpy as np
+from numpy import linalg
+from sympy import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sin, sqrt
+
+def F1(x1, x2, x3):
+    x = np.array([x1, x2, x3])
+    a = np.array([0.361522, 0.56793, 0.466301])
+    R = 0.5    
+    x_a = x-a
+    x_a = np.sum((x_a)**2)
+    # return x_a - R**2
+    return x3**2 - x1**2 - x2**2
+
+
+def F2(x1, x2, x3):
+    x = np.array([x1, x2, x3])
+    a = np.array([0.902117, 0.33316, 0.556237])
+    R = 0.5    
+    x_a = x-a
+    x_a = np.sum((x_a)**2)
+    # return x_a - R**2
+    return 1 - x3**2 - x2**2
+
+def F1_curve(l1):
+    return (F1(l1[0],l1[1],l1[2]))**2
+
+def F2_curve(l1):
+    return (F2(l1[0],l1[1],l1[2]))**2
+
+def G(l1):
+    return (F1(l1[0],l1[1],l1[2]))**2 + (F2(l1[0],l1[1],l1[2]))**2
+
+x1 = Symbol('x')
+x2 = Symbol('y')
+x3 = Symbol('z')
+X = Matrix([x1, x2, x3])
+y = G(X)
+
+yprime1 = y.diff(x1)
+yprime2 = y.diff(x2)
+yprime3 = y.diff(x3)
+yprime_mat = Matrix([yprime1,yprime2,yprime3])
+call_yprime_mat = lambdify((x1, x2,x3), yprime_mat, 'numpy')
+call_y = lambdify((x1, x2,x3), y, 'numpy')
+
+
+y_F1 = F1_curve(X)
+y_F1_prime1 = y_F1.diff(x1)
+y_F1_prime2 = y_F1.diff(x2)
+y_F1_prime3 = y_F1.diff(x3)
+y_F1_prime_mat = Matrix([y_F1_prime1,y_F1_prime2,y_F1_prime3])
+call_y_F1_prime_mat = lambdify((x1, x2,x3), y_F1_prime_mat, 'numpy')
+call_y_F1 = lambdify((x1, x2,x3), y_F1, 'numpy')
+
+
+def generate_F1_sample():
+    lr = 0.01
+    # x = np.random.random(3)
+    x = np.random.uniform(-2, 2, 3)
+    G_value = call_y_F1(x[0], x[1], x[2])
+    while(G_value > 1.0e-10):
+        grad = call_y_F1_prime_mat(x[0], x[1], x[2])
+        x = x - (lr*grad).reshape((3,))
+        G_value = call_y_F1(x[0], x[1], x[2])    
+    return x
+
+
+y_F2 = F2_curve(X)
+y_F2_prime1 = y_F2.diff(x1)
+y_F2_prime2 = y_F2.diff(x2)
+y_F2_prime3 = y_F2.diff(x3)
+y_F2_prime_mat = Matrix([y_F2_prime1,y_F2_prime2,y_F2_prime3])
+call_y_F2_prime_mat = lambdify((x1, x2,x3), y_F2_prime_mat, 'numpy')
+call_y_F2 = lambdify((x1, x2,x3), y_F2, 'numpy')
+
+
+def generate_F2_sample():
+    lr = 0.01
+    # x = np.random.random(3)
+    x = np.random.uniform(-2, 2, 3)
+    G_value = call_y_F2(x[0], x[1], x[2])
+    while(G_value > 1.0e-10):
+        grad = call_y_F2_prime_mat(x[0], x[1], x[2])
+        x = x - (lr*grad).reshape((3,))
+        G_value = call_y_F2(x[0], x[1], x[2])    
+    return x
+
+
+
+def generate_sample():
+    lr = 0.01
+    # x = np.random.random(3)
+    x = np.random.uniform(-2, 2, 3)
+    G_value = call_y(x[0], x[1], x[2])
+    while(G_value > 1.0e-10):
+        grad = call_yprime_mat(x[0], x[1], x[2])
+        x = x - (lr*grad).reshape((3,))
+        G_value = call_y(x[0], x[1], x[2])    
+    return x
+
+
