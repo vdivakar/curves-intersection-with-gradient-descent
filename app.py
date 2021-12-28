df6b85b2b350f3ab92544a14facdeb30cbd9ac43
Divakar Verma <divakarv96@gmail.com>
2021-12-28 16:17:16 -0500

    1. Multiple fxns 2. Slider for Num of Points 3. Decreased threshold for faster performance

--- app.py
+++ app.py
@@ -2,16 +2,30 @@ import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
 from utils import *
 
 st.set_page_config(layout="wide")
 
+col1,col2,col3,col4 = st.columns([1,2,2,4])
+with col1:
+    func1 = st.radio("Function-1", ('DoubleCone1', 'Sphere1', 'Cylinder1'))
+    func1 = str(func1)
+with col2:
+    func2 = st.radio("Function-2", ('DoubleCone2', 'Sphere2', 'Cylinder2'))
+    func2 = str(func2)
+with col3:
+    N = st.slider('Number of points to plot. More the slower', 200, 3000, 500, step=100)
+with col4:
+    st.write(F1_curve(X, func1))
+    st.write(F2_curve(X, func2))
+    # st.write(G(X,func1,func2))
+
 col1,col2 = st.columns(2)
 
-N = 500
-data1 = np.array([list(generate_F1_sample()) for i in range(N)])
-data2 = np.array([list(generate_F2_sample()) for i in range(N)])
+
+# N = 5000
+data1 = generate_F1_sample(N, func1)
+data2 = generate_F2_sample(N, func2)
 
 x, y, z = data1[:, 0], data1[:, 1], data1[:, 2]
 trace1 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F1", marker=dict(
@@ -27,25 +41,20 @@ fig.add_trace(trace1)
 fig.add_trace(trace2)
 fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
 
-fig.update_layout(height=800, width=1000)
+fig.update_layout(height=600, width=1000)
 with col1:
-    st.plotly_chart(fig, use_container_width=True, height = 800, width=1000)
+    st.plotly_chart(fig, use_container_width=True, height=600, width=1000)
 
 ## Plot for Intersection of Curves
-data = [list(generate_sample()) for i in range(N)]
+data = generate_intersection_sample(N, func1,func2)
 data = np.array(data)
 x, y, z = data[:, 0], data[:, 1], data[:, 2]
 fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z,mode='markers',marker=dict(
             size=5,sizemode='diameter'))])
-fig.update_layout(height=800, width=800)
+fig.update_layout(height=600, width=800)
 fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
 
 with col2:
-    st.plotly_chart(fig, use_container_width=True, height = 800, width=800)
+    st.plotly_chart(fig, use_container_width=True, height=600, width=800)
 
 
-st.write(F1_curve(X))
-st.write(F2_curve(X))
-st.write(G(X))
-
-st.write(y_F1_prime_mat)
--- utils.py
+++ utils.py
@@ -1,100 +1,117 @@
 from sympy import Symbol, Matrix, lambdify
 import numpy as np
 
-def F1(x1, x2, x3):
+def F1(x1, x2, x3, f1_type):
     x = np.array([x1, x2, x3])
-    a = np.array([0.361522, 0.56793, 0.466301])
-    R = 0.5    
-    x_a = x-a
-    x_a = np.sum((x_a)**2)
-    # return x_a - R**2
-    return x3**2 - x1**2 - x2**2
+    if f1_type == "DoubleCone1":
+        return x3**2 - x1**2 - x2**2
+    elif f1_type == "Cylinder1":
+        return 1 - x3**2 - x2**2
+    elif f1_type == "Sphere1":
+        a = np.array([0.36, 0.56, 0.46])
+        R = 0.5    
+        x_a = x-a
+        x_a = np.sum((x_a)**2)
+        return x_a - R**2
+    # return x3**2 - x1**2 - x2**2
 
 
-def F2(x1, x2, x3):
+def F2(x1, x2, x3, f2_type):
     x = np.array([x1, x2, x3])
-    a = np.array([0.902117, 0.33316, 0.556237])
-    R = 0.5    
-    x_a = x-a
-    x_a = np.sum((x_a)**2)
-    # return x_a - R**2
-    return 1 - x3**2 - x2**2
 
-def F1_curve(l1):
-    return (F1(l1[0],l1[1],l1[2]))**2
+    if f2_type == "DoubleCone2":
+        return (x2-0.90)**2 - x1**2 - x3**2
+    elif f2_type == "Cylinder2":
+        return 1 - x1**2 - x2**2
+    elif f2_type == "Sphere2":
+        a = np.array([0.90, 0.33, 0.55])
+        R = 0.5    
+        x_a = x-a
+        x_a = np.sum((x_a)**2)
+        return x_a - R**2
+    
+    # return 1 - x3**2 - x2**2
 
-def F2_curve(l1):
-    return (F2(l1[0],l1[1],l1[2]))**2
+def F1_curve(l1, f1_type):
+    return (F1(l1[0],l1[1],l1[2], f1_type))**2
 
-def G(l1):
-    return (F1(l1[0],l1[1],l1[2]))**2 + (F2(l1[0],l1[1],l1[2]))**2
+def F2_curve(l1, f2_type):
+    return (F2(l1[0],l1[1],l1[2],f2_type))**2
+
+def G(l1, f1_type, f2_type):
+    return (F1(l1[0],l1[1],l1[2],f1_type))**2 + (F2(l1[0],l1[1],l1[2],f2_type))**2
 
 x1 = Symbol('x')
 x2 = Symbol('y')
 x3 = Symbol('z')
 X = Matrix([x1, x2, x3])
-y = G(X)
 
-yprime1 = y.diff(x1)
-yprime2 = y.diff(x2)
-yprime3 = y.diff(x3)
-yprime_mat = Matrix([yprime1,yprime2,yprime3])
-call_yprime_mat = lambdify((x1, x2,x3), yprime_mat, 'numpy')
-call_y = lambdify((x1, x2,x3), y, 'numpy')
+def generate_F1_sample(N, f1_type):
+    y_F1 = F1_curve(X, f1_type)
+    y_F1_prime1 = y_F1.diff(x1)
+    y_F1_prime2 = y_F1.diff(x2)
+    y_F1_prime3 = y_F1.diff(x3)
+    y_F1_prime_mat = Matrix([y_F1_prime1,y_F1_prime2,y_F1_prime3])
+    call_y_F1_prime_mat = lambdify((x1,x2,x3), y_F1_prime_mat, 'numpy')
+    call_y_F1 = lambdify((x1, x2,x3, f1_type), y_F1, 'numpy')
 
-
-y_F1 = F1_curve(X)
-y_F1_prime1 = y_F1.diff(x1)
-y_F1_prime2 = y_F1.diff(x2)
-y_F1_prime3 = y_F1.diff(x3)
-y_F1_prime_mat = Matrix([y_F1_prime1,y_F1_prime2,y_F1_prime3])
-call_y_F1_prime_mat = lambdify((x1, x2,x3), y_F1_prime_mat, 'numpy')
-call_y_F1 = lambdify((x1, x2,x3), y_F1, 'numpy')
-
-
-def generate_F1_sample():
     lr = 0.01
-    # x = np.random.random(3)
-    x = np.random.uniform(-2, 2, 3)
-    G_value = call_y_F1(x[0], x[1], x[2])
-    while(G_value > 1.0e-10):
-        grad = call_y_F1_prime_mat(x[0], x[1], x[2])
-        x = x - (lr*grad).reshape((3,))
-        G_value = call_y_F1(x[0], x[1], x[2])    
-    return x
+    samples = []
+    for i in range(N):
+        x = np.random.uniform(-2, 2, 3)
+        G_value = call_y_F1(x[0], x[1], x[2], f1_type)
+        while(G_value > 1.0e-7):
+            grad = call_y_F1_prime_mat(x[0], x[1], x[2])
+            x = x - (lr*grad).reshape((3,))
+            G_value = call_y_F1(x[0], x[1], x[2],f1_type) 
+        samples.append(x)   
+    return np.array(samples)
 
 
-y_F2 = F2_curve(X)
-y_F2_prime1 = y_F2.diff(x1)
-y_F2_prime2 = y_F2.diff(x2)
-y_F2_prime3 = y_F2.diff(x3)
-y_F2_prime_mat = Matrix([y_F2_prime1,y_F2_prime2,y_F2_prime3])
-call_y_F2_prime_mat = lambdify((x1, x2,x3), y_F2_prime_mat, 'numpy')
-call_y_F2 = lambdify((x1, x2,x3), y_F2, 'numpy')
+def generate_F2_sample(N, f2_type):
+    y_F2 = F2_curve(X,f2_type)
+    y_F2_prime1 = y_F2.diff(x1)
+    y_F2_prime2 = y_F2.diff(x2)
+    y_F2_prime3 = y_F2.diff(x3)
+    y_F2_prime_mat = Matrix([y_F2_prime1,y_F2_prime2,y_F2_prime3])
+    call_y_F2_prime_mat = lambdify((x1, x2,x3), y_F2_prime_mat, 'numpy')
+    call_y_F2 = lambdify((x1, x2,x3,f2_type), y_F2, 'numpy')
 
-
-def generate_F2_sample():
     lr = 0.01
-    # x = np.random.random(3)
-    x = np.random.uniform(-2, 2, 3)
-    G_value = call_y_F2(x[0], x[1], x[2])
-    while(G_value > 1.0e-10):
-        grad = call_y_F2_prime_mat(x[0], x[1], x[2])
-        x = x - (lr*grad).reshape((3,))
-        G_value = call_y_F2(x[0], x[1], x[2])    
-    return x
+    samples = []
+    for i in range(N):
+        x = np.random.uniform(-2, 2, 3)
+        G_value = call_y_F2(x[0], x[1], x[2],f2_type)
+        while(G_value > 1.0e-7):
+            grad = call_y_F2_prime_mat(x[0], x[1], x[2])
+            x = x - (lr*grad).reshape((3,))
+            G_value = call_y_F2(x[0], x[1], x[2],f2_type)    
+        samples.append(x)
+    return np.array(samples)
 
 
 
-def generate_sample():
+def generate_intersection_sample(N, f1_type, f2_type):
+
+    y = G(X,f1_type, f2_type)
+
+    yprime1 = y.diff(x1)
+    yprime2 = y.diff(x2)
+    yprime3 = y.diff(x3)
+    yprime_mat = Matrix([yprime1,yprime2,yprime3])
+    call_yprime_mat = lambdify((x1, x2,x3), yprime_mat, 'numpy')
+    call_y = lambdify((x1, x2,x3,f1_type, f2_type), y, 'numpy')
+
     lr = 0.01
-    # x = np.random.random(3)
-    x = np.random.uniform(-2, 2, 3)
-    G_value = call_y(x[0], x[1], x[2])
-    while(G_value > 1.0e-10):
-        grad = call_yprime_mat(x[0], x[1], x[2])
-        x = x - (lr*grad).reshape((3,))
-        G_value = call_y(x[0], x[1], x[2])    
-    return x
+    samples = []
+    for i in range(N):
+        x = np.random.uniform(-2, 2, 3)
+        G_value = call_y(x[0], x[1], x[2],f1_type, f2_type)
+        while(G_value > 1.0e-7):
+            grad = call_yprime_mat(x[0], x[1], x[2])
+            x = x - (lr*grad).reshape((3,))
+            G_value = call_y(x[0], x[1], x[2], f1_type, f2_type)  
+        samples.append(x)  
+    return np.array(samples)
 
 
