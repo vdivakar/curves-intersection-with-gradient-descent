8a53cbfd1c84cfbd4c11e5a67752aeffbd3f9f33
Divakar Verma <divakarv96@gmail.com>
2022-01-03 23:05:35 -0500

    code cleanup. Add comments

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from utils import *
 
+# set default page config to wide
 st.set_page_config(layout="wide")
 
+# Title text of the app
+title_txt = "Plotting intersection of curves using gradient descent"
+st.markdown(f"<h3 style='text-align: center;'>{title_txt}</h1> <br><br>", unsafe_allow_html=True)
+
+# Next row of app will have 4 columns
+#   column-1 : Input for function-1
+#   column-2 : Input for function-2
+#   column-3 : Input for no. of points to plot
+#   column-4 : Display equations of functions
 col1,col2,col3,col4 = st.columns([1,2,2,4])
 with col1:
     func1 = st.radio("Function-1", ('DoubleCone1', 'Sphere1', 'Cylinder1'))
@@ -18,37 +28,38 @@ with col3:
 with col4:
     st.write(F1_curve(X, func1))
     st.write(F2_curve(X, func2))
-    # st.write(G(X,func1,func2))
 
+# Next row of app will have 2 columns
+#   column-1 : Plot for input functions
+#   column-2 : Plot for intersection of the functions
 col1,col2 = st.columns(2)
 
-data1 = generate_F1_sample(N, func1)
-data2 = generate_F2_sample(N, func2)
-
-x, y, z = data1[:, 0], data1[:, 1], data1[:, 2]
-trace1 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F1", marker=dict(size=5,sizemode='diameter'))
-x, y, z = data2[:, 0], data2[:, 1], data2[:, 2]
-trace2 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F2", marker=dict(size=5,sizemode='diameter'))
-
-fig = make_subplots()
-fig.add_trace(trace1)
-fig.add_trace(trace2)
-fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-
-fig.update_layout(height=600, width=1000)
 with col1:
+    data1 = generate_F1_sample(N, func1)
+    data2 = generate_F2_sample(N, func2)
+
+    x, y, z = data1[:, 0], data1[:, 1], data1[:, 2]
+    trace1 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F1", marker=dict(size=5,sizemode='diameter'))
+    x, y, z = data2[:, 0], data2[:, 1], data2[:, 2]
+    trace2 = go.Scatter3d(x=x, y=y, z=z,mode='markers', name="F2", marker=dict(size=5,sizemode='diameter'))
+
+    fig = make_subplots()
+    fig.add_trace(trace1)
+    fig.add_trace(trace2)
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), height=600, width=1000)
     st.plotly_chart(fig, use_container_width=True, height=600, width=1000)
 
-## Plot for Intersection of Curves
-data = generate_intersection_sample(N, func1,func2)
-data = np.array(data)
-x, y, z = data[:, 0], data[:, 1], data[:, 2]
-fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z,mode='markers',marker=dict(
-            size=5,sizemode='diameter'))])
-fig.update_layout(height=600, width=800)
-fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-
 with col2:
+    ## Plot for Intersection of Curves
+    data = generate_intersection_sample(N, func1,func2)
+    data = np.array(data)
+    x, y, z = data[:, 0], data[:, 1], data[:, 2]
+    
+    fig = go.Figure(data=[
+            go.Scatter3d(x=x, y=y, z=z,mode='markers',marker=dict(size=5,sizemode='diameter'))]
+        )
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), height=600, width=800)
     st.plotly_chart(fig, use_container_width=True, height=600, width=800)
+
 
 
--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@ from sympy import Symbol, Matrix, lambdify
 import numpy as np
 from sympy.tensor.array import derive_by_array
 
+# defining sympy variables for equations
 x1 = Symbol('x')
 x2 = Symbol('y')
 x3 = Symbol('z')
 X = Matrix([x1, x2, x3])
 
+# equation of Fxn-1 depending upon the input
 def F1(x1, x2, x3, f1_type):
     x = np.array([x1, x2, x3])
     if f1_type == "DoubleCone1":
@@ -20,6 +22,7 @@ def F1(x1, x2, x3, f1_type):
         x_a = np.sum((x_a)**2)
         return x_a - R**2
 
+# equation of Fxn-2 depending upon the input
 def F2(x1, x2, x3, f2_type):
     x = np.array([x1, x2, x3])
     if f2_type == "DoubleCone2":
@@ -33,15 +36,23 @@ def F2(x1, x2, x3, f2_type):
         x_a = np.sum((x_a)**2)
         return x_a - R**2
 
+# Graph of input functions is F(x) = 0
+# Minimise (F(x))^2 to 0 using gradient descent
 def F1_curve(l1, f1_type):
     return (F1(l1[0],l1[1],l1[2], f1_type))**2
 
 def F2_curve(l1, f2_type):
     return (F2(l1[0],l1[1],l1[2],f2_type))**2
 
+# Graph of intersection is G(x) = (F1(x))^2 + (F2(x))^2
 def G(l1, f1_type, f2_type):
     return (F1(l1[0],l1[1],l1[2],f1_type))**2 + (F2(l1[0],l1[1],l1[2],f2_type))**2
 
+# Generate N points of fxn-1
+# To speed up: Instead of using a fixed learning-rate, 
+# I've used optimised value for each step.
+# It involves Hessian (double derivate of function).
+# Separate post for it later. You can use fixed lr too.
 def generate_F1_sample(N, f1_type):
     y_F1 = F1_curve(X, f1_type)
 
@@ -59,18 +70,21 @@ def generate_F1_sample(N, f1_type):
     call_y_F1 = lambdify((x1, x2,x3, f1_type), y_F1, 'numpy')
 
     samples = []
-    for i in range(N):
+    for _ in range(N):
+        # Generate a random point
         x = np.random.uniform(-2, 2, 3)
-        G_value = call_y_F1(x[0], x[1], x[2], f1_type)
-        while(G_value > 1.0e-7):
+        F1_value = call_y_F1(x[0], x[1], x[2], f1_type)
+        # Repeate until value of fxn is ~ zero
+        while(F1_value > 1.0e-7):
             grad = call_y_F1_prime_mat(x[0], x[1], x[2])
             lr = call_numerator(x[0], x[1], x[2]) / call_denominator(x[0], x[1], x[2])
+            # update x using gradient descent
             x = x - (lr*grad).reshape((3,))
-            G_value = call_y_F1(x[0], x[1], x[2],f1_type) 
+            F1_value = call_y_F1(x[0], x[1], x[2],f1_type) 
         samples.append(x)   
     return np.array(samples)
 
-
+# Similar as above function
 def generate_F2_sample(N, f2_type):
     y_F2 = F2_curve(X,f2_type)
 
@@ -88,17 +102,19 @@ def generate_F2_sample(N, f2_type):
     call_y_F2 = lambdify((x1, x2,x3,f2_type), y_F2, 'numpy')
 
     samples = []
-    for i in range(N):
+    for _ in range(N):
         x = np.random.uniform(-2, 2, 3)
-        G_value = call_y_F2(x[0], x[1], x[2],f2_type)
-        while(G_value > 1.0e-7):
+        F2_value = call_y_F2(x[0], x[1], x[2],f2_type)
+        while(F2_value > 1.0e-7):
             grad = call_y_F2_prime_mat(x[0], x[1], x[2])
             lr = call_numerator(x[0], x[1], x[2]) / call_denominator(x[0], x[1], x[2])
             x = x - (lr*grad).reshape((3,))
-            G_value = call_y_F2(x[0], x[1], x[2],f2_type)    
+            F2_value = call_y_F2(x[0], x[1], x[2],f2_type)    
         samples.append(x)
     return np.array(samples)
 
+# Using vanilla gradient descent.
+# [To Do] Optimised alpha not giving proper convergence
 def generate_intersection_sample(N, f1_type, f2_type):
 
     y = G(X,f1_type, f2_type)
@@ -124,7 +140,7 @@ def generate_intersection_sample(N, f1_type, f2_type):
             grad = call_yprime_mat(x[0], x[1], x[2])
             # lr = call_numerator(x[0], x[1], x[2]) / call_denominator(x[0], x[1], x[2])
             # print(lr)
-            lr = 0.02
+            lr = 0.02 # Using Fixed alpha here
             x = x - (lr*grad).reshape((3,))
             G_value = call_y(x[0], x[1], x[2], f1_type, f2_type)  
         samples.append(x)  
